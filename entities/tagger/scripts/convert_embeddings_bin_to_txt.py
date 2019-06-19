ab2174e5519f1bfb263c8f15f5a768f548a2a6a0
David Bamman <david.bamman@gmail.com>
2019-06-19 07:18:59 -0700

    removed old entity tagger

--- entities/tagger/scripts/convert_embeddings_bin_to_txt.py
+++
@@ -1,6 +0,0 @@
-import gensim.models
-from gensim.models import word2vec
-import sys
-
-model = gensim.models.KeyedVectors.load_word2vec_format(sys.argv[1], binary=True)
-model.save_word2vec_format(sys.argv[2], binary=False)
